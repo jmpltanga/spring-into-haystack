1d061f589cc27ce267072100a5bf7dc9eaef0b88
jmpltanga <128316983+jmpltanga@users.noreply.github.com>
2025-04-20 01:11:05 +0000

    issue created

--- src/github-agent.py
+++ src/github-agent.py
@@ -2,33 +2,47 @@ from haystack.components.generators.chat import OpenAIChatGenerator
 from haystack.dataclasses import ChatMessage
 from haystack.components.agents import Agent
 from haystack_integrations.tools.mcp import MCPTool, StdioServerInfo
-
+from haystack.utils import Secret
+import os
 
 github_mcp_server = StdioServerInfo(
         ## TODO: Add correct params for the Github MCP Server (official or legacy)
-        env={
-            "GITHUB_PERSONAL_ACCESS_TOKEN": "<YOUR GITHUB TOKEN>" # DO NOT ADD IT TO YOUR COMMIT
+        command = "../github-mcp-server/github-mcp-server",
+        args = ["stdio"],
+        env = {
+            "GITHUB_PERSONAL_ACCESS_TOKEN": os.getenv("GITHUB_PERSONAL_ACCESS_TOKEN"), # DO NOT ADD IT TO YOUR COMMIT
+            "GITHUB_ENABLE_COMMAND_LOGGING": "true"
         }
     )
 
 print("MCP server is created")
 
 ## TODO: Create your tools here:
-tool_1 = MCPTool(...
 
-tools = [## TODO: Add tools tool_1, tool_2, tool_3,..]
+tool_list_files = MCPTool(name="search_code", server_info=github_mcp_server)
+tool_read_file = MCPTool(name="get_file_contents", server_info=github_mcp_server)
+tool_create_issue = MCPTool(name="create_issue", server_info=github_mcp_server)
+
+tools = [tool_list_files, tool_read_file, tool_create_issue]
 
 print("MCP tools are created")
 
 ## TODO: Create your Agent here:
+
+generator = OpenAIChatGenerator(
+    api_key = Secret.from_token(os.getenv("api_key")),
+    model = "o4-mini"
+)
+
 agent = Agent(
-...
+    tools = tools,
+    chat_generator = generator,
 )
 
 print("Agent created")
 
 ## Example query to test your agent
-user_input = "Can you find the typo in the README of <owner/repo> and open an issue about how to fix it?"
+user_input = "Can you find the typo in the README of jmpltanga/spring-into-haystack and open an issue about how to fix it?"
 
 ## (OPTIONAL) Feel free to add other example queries that can be resolved with this Agent
 
